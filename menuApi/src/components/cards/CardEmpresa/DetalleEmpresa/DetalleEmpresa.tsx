8755c94b3c2700af9eee8c7e2c17fcfe09bb6d39
OrnelaCondori <ornelasaracamilacondori@gmail.com>
2024-11-02 17:20:04 -0300

    cambios en los estilos de empresa popUps

--- menuApi/src/components/cards/CardEmpresa/DetalleEmpresa/DetalleEmpresa.tsx
+++ menuApi/src/components/cards/CardEmpresa/DetalleEmpresa/DetalleEmpresa.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import "./DetalleEmpresa.css";
 import { IEmpresa2 } from '../../../../types/dtos/empresa/IEmpresa2';
-import './DetalleEmpresa.css'
 
 interface DetalleEmpresaProps {
   empresa: IEmpresa2;
@@ -10,17 +9,6 @@ interface DetalleEmpresaProps {
 
 const DetalleEmpresa: React.FC<DetalleEmpresaProps> = ({ empresa, onClose }) => {
   return (
-<<<<<<< HEAD
-    <div className="modal_contenedor">
-      <h2 className='modal_titulo'>Detalle de Empresa</h2>
-      <p>Nombre: {empresa.nombre}</p>
-      <p>Razón Social: {empresa.razonSocial}</p>
-      <p>CUIT: {empresa.cuit}</p>
-      <div className='modal_botones_contenedor'>
-        <button onClick={onClose}>Cerrar</button>
-      </div>
-      
-=======
     <div className="detalleModal-fondo">
       <div className="detalleModal-contenedor">
         <h2 className='detalleModal__titulo'>Detalle de Empresa</h2>
@@ -30,7 +18,6 @@ const DetalleEmpresa: React.FC<DetalleEmpresaProps> = ({ empresa, onClose }) => 
         <p><img className='detalleModal__imagen' src={empresa.logo} alt="" /></p>
         <button className='detalleModal__boton' onClick={onClose}>Cerrar</button>
       </div>
->>>>>>> 49a125d0532121893d3c51d8e8dc6715420de876
     </div>
   );
 };
