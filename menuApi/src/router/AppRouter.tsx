2e685e90a211726d098fa3e0e67f2e3f12934493
unknown <mranaldi@gmail.com>
2024-11-08 20:20:08 -0300

    ..... 2

--- menuApi/src/router/AppRouter.tsx
+++ menuApi/src/router/AppRouter.tsx
@@ -1,4 +1,4 @@
-import { Route, Routes } from "react-router-dom";
+import { Navigate, Route, Routes } from "react-router-dom";
 import { EmpresaPage } from "../pages/EmpresaPage/EmpresaPage";
 import { MenuPage } from "../pages/MenuPage/MenuPage";
 
@@ -7,6 +7,7 @@ export const AppRouter = () => {
   return (
     <>
       <Routes>
+      <Route path="/" element={<Navigate to="/principal" />} />
         <Route path="/principal" element={<EmpresaPage />} />
         <Route path="/administracion" element={<MenuPage />} />
       </Routes>
